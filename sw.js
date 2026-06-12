ac98df447983e10ae1aeee8e52d284e3a5ea7a6c
tspr-tc-literacy <tspr@tc.vic.edu.au>
2026-06-12 12:49:29 +1000

    Publish sw.js (Phase 1, v1.1.0)

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'spelling-app-v1.0.0';
+const ASSETS = ['./', './index.html', './manifest.webmanifest'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true })
+      .then(hit => hit || fetch(e.request))
+  );
+});
